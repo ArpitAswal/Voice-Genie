62fb1ee0871bcb12fd801dc74fb44593ee941cef
ArpitAswal <arpitaswal995@gmail.com>
2025-01-20 00:00:17 +0530

    changes done

--- lib/widgets/prompt_container_child.dart
+++ lib/widgets/prompt_container_child.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
-import 'package:shimmer/shimmer.dart';
 import 'package:voice_assistant/presentation/controllers/home_controller.dart';
 import 'package:voice_assistant/widgets/prompt_messages.dart';
 
@@ -12,34 +11,20 @@ class PromptContainerChild extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        PromptMessagesWidget(ctrl: ctrl, message: ctrl.messages),
-        Obx(
-          () => (!ctrl.isLoading.value)
+    return Obx(
+      () => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          PromptMessagesWidget(ctrl: ctrl, message: ctrl.messages),
+          (!ctrl.isLoading.value)
               ? const SizedBox.shrink()
-              : (ctrl.callImagine.value)
-                  ? Align(
-                      alignment: Alignment.centerRight,
-                      child: Shimmer.fromColors(
-                          baseColor: Colors.grey.shade200,
-                          highlightColor: Colors.grey.shade400,
-                          child: Container(
-                              height: Get.height * 0.3,
-                              width: Get.width * 0.5,
-                              margin: const EdgeInsets.only(right: 12.0),
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(16.0),
-                              ))))
-                  : Align(
-                      alignment: Alignment.centerLeft,
-                      child: LoadingAnimationWidget.progressiveDots(
-                          color: Colors.grey.shade400, size: 40),
-                    ),
-        )
-      ],
+              : Align(
+                  alignment: Alignment.centerLeft,
+                  child: LoadingAnimationWidget.progressiveDots(
+                      color: Colors.grey.shade400, size: 40),
+                ),
+        ],
+      ),
     );
   }
 }
